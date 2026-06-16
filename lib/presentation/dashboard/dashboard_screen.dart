dca22ff4e876384ef9ee96289ad017c96399203a
Satish Soni <sonisatish119@gmail.com>
2026-06-16 12:20:45 +0530

    expense add

--- lib/presentation/dashboard/dashboard_screen.dart
+++ lib/presentation/dashboard/dashboard_screen.dart
@@ -64,7 +64,7 @@ class DashboardScreen extends StatefulWidget {
 
 class _DashboardScreenState extends State<DashboardScreen> {
   // --- APP VERSION (Update this manually before every new build) ---
-  static const String CURRENT_APP_VERSION = "1.0.37";
+  static const String CURRENT_APP_VERSION = "1.0.38";
 
   // --- STATE ---
   bool _isCheckedIn = false;
