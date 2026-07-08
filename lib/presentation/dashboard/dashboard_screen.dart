90c651a38c80a00176a1deb1f45a55796628b932
omkarpehere <omkar.p@globalspace.in>
2026-07-08 14:49:27 +0530

    updated support email

--- lib/presentation/dashboard/dashboard_screen.dart
+++ lib/presentation/dashboard/dashboard_screen.dart
@@ -66,7 +66,7 @@ class DashboardScreen extends StatefulWidget {
 
 class _DashboardScreenState extends State<DashboardScreen> {
   // --- APP VERSION (Update this manually before every new build) ---
-  static const String CURRENT_APP_VERSION = "1.0.54";
+  static const String CURRENT_APP_VERSION = "1.0.55";
 
   // --- STATE ---
   bool _isCheckedIn = false;
