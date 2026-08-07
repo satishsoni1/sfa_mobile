5dbf4b6567eafdcc22067c284edf8fd71cbf682f
omkarpehere <omkar.p@globalspace.in>
2026-08-07 19:21:39 +0530

    sending amount in fixed montly expene

--- lib/presentation/dashboard/dashboard_screen.dart
+++ lib/presentation/dashboard/dashboard_screen.dart
@@ -70,7 +70,7 @@ class DashboardScreen extends StatefulWidget {
 
 class _DashboardScreenState extends State<DashboardScreen> {
   // --- APP VERSION (Update this manually before every new build) ---
-  static const String CURRENT_APP_VERSION = "1.0.67";
+  static const String CURRENT_APP_VERSION = "1.0.68";
 
   // --- STATE ---
   bool _isCheckedIn = false;
