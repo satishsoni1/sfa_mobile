db7f732554305d0c0193995bd9d6e9615be89f85
omkarpehere <omkar.p@globalspace.in>
2026-09-02 09:41:21 +0530

    version increased

--- lib/presentation/dashboard/dashboard_screen.dart
+++ lib/presentation/dashboard/dashboard_screen.dart
@@ -69,7 +69,7 @@ class DashboardScreen extends StatefulWidget {
 
 class _DashboardScreenState extends State<DashboardScreen> {
   // --- APP VERSION (Update this manually before every new build) ---
-  static const String CURRENT_APP_VERSION = "1.0.65";
+  static const String CURRENT_APP_VERSION = "1.0.66";
 
   // --- STATE ---
   bool _isCheckedIn = false;
